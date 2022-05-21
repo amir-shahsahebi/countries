3d4cca9a59799b6f87f77cc68f313ba55da688bf
amir shahsahebi <shahsahebiamir@gmail.com>
2022-05-21 19:46:24 +0430

    variable color added

--- src/Navbar.js
+++ src/Navbar.js
@@ -2,7 +2,14 @@ const Navbar = () => {
   return (
     <nav>
       <h1>Where is the world?</h1>
-      <span>
+      <span
+        style={{
+          root: {
+            __acolor: "red",
+          },
+        }}
+        className="switch"
+      >
         <i className="fas fa-sun"></i>
         <span>Light Mode</span>
       </span>
