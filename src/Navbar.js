ab45680c96bfe929a58a5e33263053f60df85135
amir shahsahebi <shahsahebiamir@gmail.com>
2022-05-21 17:19:40 +0430

    fonts added

---
+++ src/Navbar.js
@@ -0,0 +1,13 @@
+const Navbar = () => {
+  return (
+    <nav>
+      <h1>Where is the world?</h1>
+      <span>
+        <i className="fas fa-sun"></i>
+        <span>Light Mode</span>
+      </span>
+    </nav>
+  );
+};
+
+export default Navbar;
