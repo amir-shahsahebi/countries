98fc80160eb82b2fe9878fa2a64af03cd418d57f
amir shahsahebi <shahsahebiamir@gmail.com>
2022-05-23 01:21:18 +0430

    use ref added

--- src/components/DropDown.js
+++ src/components/DropDown.js
@@ -1,15 +1,45 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 const DropDown = ({ optionList }) => {
   const [selected, setSelected] = useState(optionList[0]);
+  const [clicked, setClicked] = useState(false);
+  const ref = useRef();
+  useEffect(() => {
+    const onBodyClick = (e) => {
+      if (ref.current.contains(e.target)) {
+        return;
+      }
+      setClicked(false);
+    };
+    document.body.addEventListener("click", onBodyClick, { capture: true });
+
+    return () => {
+      document.body.removeEventListener("click", onBodyClick, {
+        capture: true,
+      });
+    };
+  }, [clicked]);
+  const handleClick = (e) => {
+    // console.dir(e.target.innerText);
+    setSelected(e.target.innerText);
+    setClicked(!clicked);
+  };
   return (
-    <div className="dropDownContainer">
-      <span className="selectedValue">{selected}</span>
-      <div className="dropDown">
+    <div ref={ref} className="dropDownContainer">
+      <span onClick={() => setClicked(!clicked)} className="selectedValue">
+        {selected}
+      </span>
+      <div className="dropDown" style={!clicked ? { display: "none" } : null}>
         <ul>
-          {optionList.map((option) => {
-            return <li>{option}</li>;
-          })}
+          {optionList
+            .filter((option) => option !== selected)
+            .map((option) => {
+              return (
+                <li key={option} onClick={handleClick}>
+                  {option}
+                </li>
+              );
+            })}
         </ul>
       </div>
     </div>
